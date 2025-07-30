a8d8aeea52182bd1ae79fec8905c8bf7d34142d8
oldgranger <carlolegaspi12@gmail.com>
2025-07-30 15:05:14 +0800

    .
    Signed-off-by: oldgranger <carlolegaspi12@gmail.com>

--- data/transforms.py
+++ data/transforms.py
@@ -7,18 +7,13 @@ def get_transform(train: bool) -> Transform:
         transforms.ToImage(),
         transforms.ToDtype(torch.float32, scale=True),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+                             std=[0.229, 0.224, 0.225]),
     ]
 
     if train:
         transform_list += [
-            transforms.RandomPhotometricDistort(),
-            # transforms.RandomIoUCrop()
-            transforms.RandomZoomOut(fill=0),
             transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomAffine(degrees=5, translate=(0.05, 0.05), scale=(0.9, 1.1)),
+            transforms.RandomPhotometricDistort(),
         ]
-
-    transform_list.append(transforms.Resize((640, 640)))
 
     return transforms.Compose(transform_list)
